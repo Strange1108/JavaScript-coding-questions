d400611609ec57c24841cee8f93eade63ed8ea89
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-05-13 16:44:10 +0530

    update callpolyfill file

--- Polyfill/callPolyfill.js
+++ Polyfill/callPolyfill.js
@@ -1,3 +1,15 @@
+// let person = {
+//   firstname: "Kirtesh",
+//   lastname: "bansal"
+// }
+
+// let printName = function (country) {
+//   console.log(this.firstname + " " + this.lastname + " from " 
+//   + country);
+// }
+
+// printName.call(person, "India");
+
 let person = {
   firstname: "Kirtesh",
   lastname: "bansal"
@@ -8,4 +20,19 @@ let printName = function (country) {
   + country);
 }
 
-printName.call(person, "India");
+Function.prototype.mycall = function(obj,...args){ 
+    let sym = Symbol();                                     
+    obj[sym] = this;
+    let res = obj[sym](...args)
+    delete obj[sym];
+    return res;
+}
+
+/*
+Note: Applying mycall method to printName function so this
+will be equal to printName inside mycall function as 
+printName is on the left side of the '.' 
+*/
+
+printName.mycall(person, "India");
+
